6220347ca6694aa6d3ad9dd7b69b0d28ffc69dd4
Nádia Cardoso de Macedo <nadia_cardoso_macedo@hotmail.com>
2026-03-12 18:31:05 +0000

    Add project files.

---
+++ Library.mvc/Models/CreateInvoiceViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Library.mvc.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using System.ComponentModel.DataAnnotations;
+
+    public class CreateInvoiceViewModel
+    {
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime InvoiceDate { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1.")]
+        public int Quantity { get; set; }
+
+        public IEnumerable<SelectListItem> Customers { get; set; }
+        public IEnumerable<SelectListItem> Products { get; set; }
+    }
+}
